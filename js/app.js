52e34a1e2003894296acffe6e7bc4dc7c1954f7b
forbrace <higher@higher.com.ua>
2015-02-01 11:11:34 +0200

    fixes

--- js/app.js
+++ js/app.js
@@ -48,9 +48,11 @@ var places = [{
     }
 }];
 
+// Place constructor
 var Place = function(data, map) {
     var self = this;
-
+    
+    // define place props
     this.map = map;
     this.isActive = ko.observable(false);
     this.name = ko.observable(data.name);
@@ -78,19 +80,23 @@ var Place = function(data, map) {
         maxWidth: 800
     });
     
+    // assing infoWindow props with curent marker & active flag
     this.infoWindow.marker = this.marker;
     this.infoWindow.isActive = this.isActive;
 
     // listen to marker clicks
     google.maps.event.addListener(self.marker, 'click', function() {
+        // show marker
         self.showInfoWindow(map, self.marker);
     });
     
     // listen to infoWindow events
     google.maps.event.addListener(self.infoWindow, 'domready', function() {
+        // show panorama when infoWindow visible
         self.showPano();
     });
     google.maps.event.addListener(self.infoWindow, 'closeclick', function() {
+        // hide panorama when close infoWindows
         self.closeAllInfoWindows();
     });
 };
@@ -100,6 +106,11 @@ Place.prototype.infoWindows = [];
 
 // close all infoWindows
 Place.prototype.closeAllInfoWindows = function() {
+    // loop all infoWindows
+    // close infoWindows
+    // set red pin icon
+    // set active flag to false
+    // hide panorama
     for (var i = 0, l = this.infoWindows.length; i < l; i++) {
         this.infoWindows[i].close();
         this.infoWindows[i].marker.setIcon('https://maps.gstatic.com/mapfiles/ms2/micons/red-dot.png');
@@ -107,21 +118,32 @@ Place.prototype.closeAllInfoWindows = function() {
     }
     this.hidePano();
 };
-
+// show infoWindow
 Place.prototype.showInfoWindow = function(map, marker) {
+    // get place details form wikipedia
+    // push this infoWindow to infoWindows array
+    // show this infoWindow and close prev. opened
+    // set blue pin icon
+     // flag as active
     this.getDetails();
     this.infoWindows.push(this.infoWindow);
     this.closeAllInfoWindows();
     this.infoWindow.open(map, marker);
     this.marker.setIcon('https://maps.gstatic.com/mapfiles/ms2/micons/blue-dot.png');
     this.isActive(true);
-    $('#loader').show();
+    // $('#loader').show();
 };
 
+// show panorama
 Place.prototype.showPano = function() {
+    // Hide any prev opened panorama
+    // Create new one
+    // Bind to position and marker
+    // And make it visible
     if (this.pano !== null) {
         this.pano.unbind("position");
         this.pano.setVisible(false);
+        this.pano = null;
     }
     this.pano = new google.maps.StreetViewPanorama(document.getElementById("pano"), {
         navigationControl: true,
@@ -133,6 +155,7 @@ Place.prototype.showPano = function() {
     this.pano.setVisible(true);
 };
 
+// Hide panorama
 Place.prototype.hidePano = function() {
     if (this.pano !== null) {
         this.pano.unbind("position");
@@ -141,10 +164,13 @@ Place.prototype.hidePano = function() {
     }
 };
 
+// Get place details from wikipedia asyncronosly with getJSON and 
+// append them to #details
 Place.prototype.getDetails = function() {
     var dataURL = "http://en.wikipedia.org/w/api.php?action=query&prop=extracts|info&format=json&exintro=&inprop=url&iwurl=&rawcontinue=&titles="+ this.name()+"&callback=?";
     $.getJSON(dataURL,function(data, textStatus, jqXHR) {
         $.each(data["query"]["pages"],function(key,value){
+            // success
             $('#details')
                 .html(value['extract'])
                 .after('<a class="wiki-link" href="'
@@ -153,19 +179,24 @@ Place.prototype.getDetails = function() {
         });
     })
     .fail(function(jqXHR, textStatus, errorThrown) {
+        // error
         $('#details').html(textStatus + ': failed to load data.');
     });
 
 };
 
+// MapViewModel constructor
 var MapViewModel = function() {
 
     var self = this;
-
+    // Define observableArray of places, 
+    // observable search query empty str,
+    // and observable current place
     self.placeList = ko.observableArray([]);
     self.query = ko.observable('');
     self.currentPlace = ko.observable();
-
+    
+    // initialize map with places
     self.initialize = function() {
 
         var mapOptions = {
@@ -180,8 +211,8 @@ var MapViewModel = function() {
             mapOptions);
 
         // loop through the places, 
-        // create place object 
-        // and push it to the placeList
+        // create place objects from constructor
+        // and push them to the placeList
         places.forEach(function(place) {
             var place = new Place(place, map);
             self.placeList.push(place);
@@ -189,23 +220,39 @@ var MapViewModel = function() {
 
     };
 
-    // show place info
+    // show place info 
     self.showInfo = function(place) {
         self.currentPlace(place);
         place.showInfoWindow(place.map, place.marker);
     };
 
-    // search places array 
-    self.places = ko.dependentObservable(function() {
+    // live search places in placeList array 
+    self.places = ko.computed(function() {
         var search = self.query().toLowerCase();
         return ko.utils.arrayFilter(self.placeList(), function(place) {
             return place.name().toLowerCase().indexOf(search) >= 0;
         });
     }, this);
 
-    // initialize map
-    google.maps.event.addDomListener(window, 'load', self.initialize);
-
 };
 
-ko.applyBindings(new MapViewModel());
+// ko.applyBindings(new MapViewModel());
+
+function init() {
+    // initialize map
+    var map = new MapViewModel();
+    google.maps.event.addDomListener(window, 'load', map.initialize);
+    ko.applyBindings(map);
+}
+
+// run init & handle error
+$.ajax({
+    url: 'https://maps.googleapis.com/maps/api/js?libraries=places&callback=init',
+    dataType: 'script',
+    success: function () {
+        // success
+    },
+    error: function () {
+        $('#log').show().find('.log-message').text('Failed to load google maps. Try to reload app later.');
+    }
+});
